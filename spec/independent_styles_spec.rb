c0e18ac4300cf243daae26e289ac7f0817bb2d85
rmzelle <rintze.zelle@gmail.com>
2016-02-22 23:20:49 -0500

    Rewrite all tests so that they work without a colon

--- spec/independent_styles_spec.rb
+++ spec/independent_styles_spec.rb
@@ -1,6 +1,6 @@
 Independents.each_pair do |basename, (filename, path, style, reason)|
 
-  describe "#{basename}:" do
+  describe "#{basename}" do
 
     it "must validate against the CSL 1.0.1 schema" do
       expect(CSL.validate(path)).to eq([])
@@ -15,11 +15,11 @@ Independents.each_pair do |basename, (filename, path, style, reason)|
     end
 
     unless style.nil?
-      it "only independent styles are allowed in the root directory" do
+      it 'must be an independent style (dependent styles must be placed in the "dependent" subdirectory)' do
         expect(style).to be_independent
       end
 
-      it "must contain an <info/> element" do
+      it "must have an <info/> element" do
        expect(style).to have_info
       end
 
@@ -31,11 +31,11 @@ Independents.each_pair do |basename, (filename, path, style, reason)|
         expect(style.info).to have_id
       end
 
-      it 'style ID must be of the form "http://www.zotero.org/styles/" + style file name (without ".csl" extension, e.g. "http://www.zotero.org/styles/apa")' do
+      it 'may only have a style ID of the form "http://www.zotero.org/styles/" + style file name (without ".csl" extension, e.g. "http://www.zotero.org/styles/apa")' do
         expect(style.id).to eq("http://www.zotero.org/styles/#{basename}")
       end
 
-      it '"self" link must match the style ID' do
+      it 'may not have a "self" link that differs from the style ID' do
         if style.has_self_link?
           expect(style.id).to eq(style.self_link)
         end
@@ -49,7 +49,7 @@ Independents.each_pair do |basename, (filename, path, style, reason)|
         expect(style).to be_default_license
       end
 
-      it '"template" link must point to an existing independent style' do
+      it 'may only have a "template" link that points to an existing independent style' do
         if style.has_template_link?
           parent_ID_link = style.template_link
           link_prefix = "http://www.zotero.org/styles/"
